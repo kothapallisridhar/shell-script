2a9e8b9cb6b84710bea28769dea7d8b6b7c521c8
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-07-17 22:23:04 +0530

    conversation

--- 02.conversation.sh
+++ 02.conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Trump: Hey Musk, how are you?
+echo "Trump: Hey Musk, how are you?"
 echo "Musk: Hello Trump, I'm fine. How are you doing?"
 echo "Trump: I'm good too. What's up?"
 echo "Musk: Nothing, just going to Mars now, will you come"
