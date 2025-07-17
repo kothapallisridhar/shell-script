977448ac97de00e55aaf99ec9fc0e3619d16fba3
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-07-17 22:22:44 +0530

    conversation

--- 02.conversation.sh
+++ 02.conversation.sh
@@ -5,5 +5,3 @@ echo "Musk: Hello Trump, I'm fine. How are you doing?"
 echo "Trump: I'm good too. What's up?"
 echo "Musk: Nothing, just going to Mars now, will you come"
 echo "Trump: Sorry, you carry on! I will come once you are back"
-
-
